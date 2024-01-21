4b8f0e514d1507762441f38f4b7bbe9bc7786dd2
sibi-tv <sibi.tiruchi@gmail.com>
2024-01-21 18:42:55 -0500

    first commit

---
+++ server_side/aracan/src/test/java/com/aracanclothing/aracan/AracanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aracanclothing.aracan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AracanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
