43f1f9266f13942c436bdf37b110051967f65344
sibi-tv <sibi.tiruchi@gmail.com>
2024-01-22 02:19:35 -0500

    Database Connection, User class

---
+++ server_side/aracan/src/main/java/com/aracanclothing/aracan/entity/User.java
@@ -0,0 +1,29 @@
+package com.aracanclothing.aracan.entity;
+
+import com.aracanclothing.aracan.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private Long phone_number;
+
+    private UserRole role;
+
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] ing;
+}
